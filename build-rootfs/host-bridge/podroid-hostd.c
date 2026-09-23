1a929622ed24f4057a45b61fac98ad9413d4a30d
nike64542-byte <nike64542-byte@users.noreply.github.com>
2026-09-23 08:41:24 +0800

    chore: dedupe sys/socket.h includes

--- build-rootfs/host-bridge/podroid-hostd.c
+++ build-rootfs/host-bridge/podroid-hostd.c
@@ -28,7 +28,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/un.h>
--- build-rootfs/vsock-agent/podroid-vsock-agent.c
+++ build-rootfs/vsock-agent/podroid-vsock-agent.c
@@ -53,7 +53,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mount.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
